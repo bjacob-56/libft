30ebb5bfa1e13ee440505597db9631c83d976cd2
bjacob-56 <bjacob56@DESKTOP-HRLC1PI.localdomain>
2020-11-15 16:49:57 +0100

    MAJ2_15-11

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,28 +3,23 @@
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
     t_list  *elem;
-    t_list  *next;
     t_list  *begin;
-    t_list  *old;
 
     if (!lst)
         return (NULL);
-    if (!(elem = malloc(sizeof(t_list))))
-            return (NULL);
-        elem->content = f(lst->content);
-        lst = lst->next;
+    if (!(elem = ft_lstnew(f(lst->content))))
+        return (NULL);
+    lst = lst->next;
     begin = elem;
     while (lst)
     {
-        if (!(next = malloc(sizeof(t_list))))
+        if (!(elem->next = ft_lstnew(f(lst->content))))
+        {
+            ft_lstclear(&begin, del);
             return (NULL);
-        next->content = f(lst->content);
-        elem->next = next;
-        elem = next;
-        old = lst;
-        del(old->content);
+        }
+        elem = elem->next;
         lst = lst->next;
-        free(old);
     }
     return (begin);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -8,23 +8,18 @@ char *ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*str;
 
-	i = 0;
-	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	i = -1;
+	j = -1;
 	l1 = ft_strlen(s1);
     l2 = ft_strlen(s2);
-    str = malloc(sizeof(char) * (l1 + l2 + 1));
-    if (!str)
-    return (NULL);
-	while (i < l1)
-	{
+    if (!(str = malloc(sizeof(char) * (l1 + l2 + 1))))
+		return (NULL);
+	while (++i < l1)
 		str[i] = s1[i];
-        i++;
-    }
-	while (j < l2)
-	{
+	while (++j < l2)
         str[i + j] = s2[j];
-        j++;
-	}
 	str[i + j] = '\0';
     return (str);
 }
